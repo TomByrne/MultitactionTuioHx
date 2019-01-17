0663304c0c96fbe169a3d129421019b4882b3add
Thomas Byrne <tom.byrne@imagination.com>
2019-01-17 14:14:26 +1100

    Migrated to 'keyboard' haxelib

--- src/imagsyd/multitaction/command/TuioDebugKeyboardCommand.hx
+++ src/imagsyd/multitaction/command/TuioDebugKeyboardCommand.hx
@@ -1,9 +1,9 @@
 package imagsyd.multitaction.command;
 
 import imagsyd.multitaction.model.touch.TuioTouchesSettingsModel;
-import openfl.ui.Keyboard;
 import robotlegs.bender.bundles.mvcs.Command;
-import imagsyd.input.IKeyboardMap;
+import keyboard.Keyboard;
+import keyboard.Key;
 
 /**
  * ...
@@ -11,16 +11,15 @@ import imagsyd.input.IKeyboardMap;
  */
 class TuioDebugKeyboardCommand extends Command
 {
-	@inject public var keyboardMap:IKeyboardMap;
 	@inject public var tuioTouchSettingsModel:TuioTouchesSettingsModel;
 	
 	public function new() { }
 	
 	override public function execute():Void
 	{
-		keyboardMap.map(handleTuioNotifierChange, Keyboard.T, { shift:true, ctrl:true } );		
-		keyboardMap.map(handleWindowsNotifierChange, Keyboard.W, { shift:true, ctrl:true } );	
-		keyboardMap.map(handleTouchDebugNotifierChange, Keyboard.D, { alt:true,shift:true, ctrl:true } );	
+        Keyboard.onPress(Key.T, handleTuioNotifierChange).shift(true).ctrl(true);
+        Keyboard.onPress(Key.W, handleWindowsNotifierChange).shift(true).ctrl(true);
+        Keyboard.onPress(Key.D, handleTouchDebugNotifierChange).shift(true).ctrl(true).alt(true);
 	}
 	
 	function handleTouchDebugNotifierChange():Void 
