01b845b526480afb51a6042a654180781c5580a2
Thomas Byrne <tom.byrne@imagination.com>
2019-08-23 10:17:01 +1000

    Typo

--- src/multitaction/logic/processors/MarkerProcessorsLogic.hx
+++ src/multitaction/logic/processors/MarkerProcessorsLogic.hx
@@ -19,7 +19,7 @@ class MarkerProcessorsLogic implements DescribedType
     {
 //		markerProcessorsModel.tuioMarkerProcessors.push( new FlipOrientationMarkerProcessor(settings.bool('tuioFlippedOrientation', false), markerObjectsModel ));
 		markerProcessorsModel.tuioMarkerProcessors.push( new SimpleMarkerFromMarkerProcessor(false, markerObjectsModel) );
-		markerProcessorsModel.tuioMarkerProcessors.push( new FlickeringFilterMarkerProcessor(true, markerObjectsModel, settings.get('tuio_safety_width'), settings.get('tuio_safety_width'), settings.get('tuio_marker_keep_alive'), settings.get('tuio_marker_retreive_olny_same_id') ));
+		markerProcessorsModel.tuioMarkerProcessors.push( new FlickeringFilterMarkerProcessor(true, markerObjectsModel, settings.get('tuio_safety_width'), settings.get('tuio_safety_width'), settings.get('tuio_marker_keep_alive'), settings.get('tuio_marker_retreive_only_same_id') ));
 		markerProcessorsModel.tuioMarkerProcessors.push( new RotateMarkersMarkerProcessor(true, markerObjectsModel, settings.get('tuioRotationOffset')));
 		markerProcessorsModel.tuioMarkerProcessors.push( new SnapAnglesMarkerProcessor(false, markerObjectsModel));
 		markerProcessorsModel.tuioMarkerProcessors.push( new FractionToPixelsMarkerProcessor(true, markerObjectsModel, multitactionSettingsModel.nativeScreenSize ) );
